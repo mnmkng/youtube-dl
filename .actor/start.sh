e23feef86f665b11c33ac694c97a549042272c8b
Ondra Urban <ondra@apify.com>
2022-04-04 15:25:28 +0200

    update script, add schema

---
+++ .actor/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+echo "Reading URL"
+URL=$(apify actor:get-input | jq -r '.url')
+
+echo "Downloading: ${URL}"
+youtube-dl "${URL}" -o >(apify actor:set-value -c 'video/mp4' VIDEO)
