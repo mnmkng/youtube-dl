636381cab70ff1db16ff46c8aa11ea300152ea5a
Ondra Urban <ondra@apify.com>
2022-04-06 11:27:28 +0200

    add info fetching and proxy

--- .actor/start.sh
+++ .actor/start.sh
@@ -5,6 +5,16 @@ set -e
 echo "Reading URL"
 URL=$(apify actor:get-input | jq -r '.url')
 
+echo "Fetching video information."
+youtube-dl  -j "${URL}" --proxy "http://auto:${APIFY_PROXY_PASSWORD}@proxy.apify.com:8000" \
+ | tee >(apify actor:set-value INFO) \
+ | cat
+
 echo "Downloading: ${URL}"
-youtube-dl "${URL}" --newline -v -o >(apify actor:set-value -c 'video/mp4' VIDEO)
-sleep 10
+youtube-dl "${URL}" \
+ --newline \
+ -v \
+ --proxy "http://auto:${APIFY_PROXY_PASSWORD}@proxy.apify.com:8000" \
+ -o >(apify actor:set-value -c 'video/mp4' VIDEO)
+# without the sleep, the video sometimes did not save
+sleep 2
